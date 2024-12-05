8e2ce58662f57f8fa8aba27c6c3afe8987705cc3
Thomas Taulo <taulo@live.dk>
2024-12-05 15:23:51 +0100

    day 5 problems solved

---
+++ Day 5 Print Queue/Problem 2/Day5.js
@@ -0,0 +1,56 @@
+import {fileReader} from '../../Helper.js'
+
+const data = await fileReader('Input.txt');
+
+const [pages, orderingRules] = data.split("\n\n")
+
+function getValidRules(currentRule) {
+    return pages.split('\n').filter((rules, index) => {
+        const [rule1, rule2] = rules.split('|')
+
+        return currentRule.includes(rule1) && currentRule.includes(rule2)
+    }).map(rule => {
+        return rule.split("|")[0]
+    })
+}
+
+function isArrayEqual(arr1, arr2) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2)
+}
+
+function countRules(currentRule, validRules) {
+    let res = []
+    for (let i = 0; i < currentRule.length; i++) {
+        let count = 0;
+        for (let j = 0; j < validRules.length; j++) {
+            if (currentRule[i] === validRules[j]) {
+                count++
+            }
+        }
+        res.push({
+            rules: currentRule[i],
+            count
+        })
+    }
+
+    return res
+}
+
+const rules = orderingRules.split("\n").map(rule => rule.split(","))
+
+const res = rules.map(rule => {
+
+    const validRules = getValidRules(rule)
+    const rulesCount = countRules(rule, validRules)
+
+    const rulesSorted = rulesCount.sort((a, b) => b.count - a.count).map((rule) => rule.rules)
+
+    if (!isArrayEqual(rulesSorted, rule)) {
+        return parseInt(rulesSorted[Math.floor(rulesSorted.length / 2)])
+    }
+
+    return 0
+}).reduce((acc, rule) => acc + rule, 0);
+
+console.log(res) // 4922
+
